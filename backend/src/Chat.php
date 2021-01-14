cde65baa7f2aca67464568240bfd71d298c60f35
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-01-13 23:26:56 -0300

    updated require path

--- backend/src/Chat.php
+++ backend/src/Chat.php
@@ -5,14 +5,14 @@
 	use SplObjectStorage;
 	use Ratchet\ConnectionInterface;
 	use Ratchet\MessageComponentInterface;
-	
+
 	final class ChatServer implements MessageComponentInterface {
 		private $clients;
-	
+
 		public function __construct() {
 			$this->clients = new SplObjectStorage();
 		}
-	
+
 		public function onOpen(ConnectionInterface $connection) : void {
 			$this->clients->attach($connection);
 			$connection->send(json_encode([
@@ -20,16 +20,16 @@
 				'message' => 'Hello, world!'
 			]));
 		}
-	
+
 		public function onMessage(ConnectionInterface $from, $msg) : void {
 			foreach ($this->clients as $client)
 				$client->send($msg);
 		}
-	
+
 		public function onClose(ConnectionInterface $connection) : void {
 			$this->clients->detach($connection);
 		}
-	
+
 		public function onError(ConnectionInterface $connection, \Exception $exception) : void {
 			$connection->close();
 		}
